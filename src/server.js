b9c76fce5fdf988606c509ad388f51ef06165536
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-03 19:28:52 -0300

    Improving code

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ require('./database/index');
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
 app.use(routes)
 
 app.listen(3333, () => console.log('Server on at port 3333'))
