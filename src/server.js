72b23b093fa5344dc920c916b5653c12cda34b54
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-03 11:36:47 -0300

    Import routes, morgan and database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const routes = require('./routes');
+const morgan = require('morgan');
+
+require('./database/index');
 
 const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(routes)
 
 app.listen(3333, () => console.log('Server on at port 3333'))
