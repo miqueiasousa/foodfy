6d0b2928ca0591a133c62c2882a63bcd453b0af1
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-03 11:37:16 -0300

    Create database config

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'foodfy',
+  password: 'foodfy',
+  database: 'foodfydb',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
