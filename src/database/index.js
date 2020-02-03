42dc9e267fc690d62279a22e5df6838870a2c4e9
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-03 19:39:27 -0300

    Initialization and associations of models

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,19 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+const User = require('../models/User');
+const Chef = require('../models/Chef');
+const Recipe = require('../models/Recipe');
+const File = require('../models/File');
+
 const connection = new Sequelize(dbConfig)
+
+User.init(connection)
+Chef.init(connection)
+Recipe.init(connection)
+File.init(connection)
+
+Chef.associate(connection.models)
+Recipe.associate(connection.models)
 
 module.exports = connection
