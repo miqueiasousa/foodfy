5d23493c43ed89e2f1d57161cc17efabf931063c
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-04 13:59:31 -0300

    Config view engine

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const nunjucks = require('nunjucks');
 const routes = require('./routes');
 
 require('./database/index');
 
 const app = express();
+
+nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+  autoescape: false,
+  express: app,
+  watch: true,
+});
+
+app.set('view engine', '.njk');
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
