ac044223f136b63d146751c095e0dfbdf8280e31
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-04 13:46:14 -0300

    Config files static

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const routes = require('./routes');
 
 require('./database/index');
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.use(routes);
 
