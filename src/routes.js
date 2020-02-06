95463f31ea22a98a0d52d8d148c2be6fa1332722
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-06 10:43:49 -0300

    Create session routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,22 @@
 const { Router } = require('express');
 
-const router = Router()
+const SessionController = require('./app/controllers/SessionController');
 
-router.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' })
-})
+const router = Router();
 
-module.exports = router
+router.get('/admin/users/login', SessionController.renderLogin);
+router.post('/admin/users/login', SessionController.login);
+router.post('/admin/users/logout', SessionController.logout);
+
+router.get(
+  '/admin/users/forgot-password',
+  SessionController.renderForgotPassword
+);
+router.post('/admin/users/forgot-password', SessionController.forgotPassword);
+router.get(
+  '/admin/users/reset-password',
+  SessionController.renderResetPassword
+);
+router.post('/admin/users/reset-password', SessionController.resetPassword);
+
+module.exports = router;
