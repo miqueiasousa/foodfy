81024976694de01c3129e7603f73338f9edd728d
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-06 20:14:36 -0300

    Add method-override

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
 const routes = require('./routes');
 const session = require('./config/session');
 
@@ -22,6 +23,7 @@ app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(methodOverride('_method'));
 app.use(session);
 app.use(
   '/files',
