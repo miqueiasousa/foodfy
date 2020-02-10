8be2fdc20e4f89fd108ee63cd941a98b3ec03366
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-09 23:54:08 -0300

    Create admin chefs routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const SessionController = require('./app/controllers/SessionController');
 const ProfileController = require('./app/controllers/ProfileController');
 const UserController = require('./app/controllers/UserController');
+const ChefController = require('./app/controllers/ChefController');
 
 const isAuthenticated = require('./app/middleware/isAuthenticated');
 const isAdmin = require('./app/middleware/isAdmin');
@@ -25,14 +26,21 @@ router.post('/admin/users/reset-password', SessionController.resetPassword);
 
 router.use(isAuthenticated);
 
-router.get('/admin/users/profile', ProfileController.index);
+router.get('/admin/users/profile', ProfileController.show);
 router.put('/admin/users/profile', ProfileController.update);
 
 router.get('/admin/users', UserController.index);
-router.get('/admin/users/:id/edit', isAdmin, UserController.edit);
 router.get('/admin/users/create', isAdmin, UserController.create);
+router.get('/admin/users/:id', isAdmin, UserController.show);
 router.post('/admin/users', isAdmin, UserController.store);
 router.put('/admin/users/:id', isAdmin, UserController.update);
 router.delete('/admin/users/:id', isAdmin, UserController.delete);
+
+router.get('/admin/chefs', ChefController.index);
+router.get('/admin/chefs/create', isAdmin, ChefController.create);
+router.get('/admin/chefs/:id', isAdmin, ChefController.show);
+router.post('/admin/chefs', isAdmin, ChefController.store);
+router.put('/admin/chefs/:id', isAdmin, ChefController.update);
+router.delete('/admin/chefs/:id', isAdmin, ChefController.delete);
 
 module.exports = router;
