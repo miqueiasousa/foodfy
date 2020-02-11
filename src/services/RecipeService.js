f462ce483159c15fbadbff4eae447b2b67ec3534
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-11 19:40:26 -0300

    Create service search by filter

--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -1,9 +1,11 @@
+const { Op } = require('sequelize');
 const Recipe = require('../models/Recipe');
 
 class RecipeService {
   static async findAll() {
     try {
       const recipes = await Recipe.findAll({
+        order: [['updated_at', 'DESC']],
         include: [{ association: 'chef' }, { association: 'files' }],
       });
 
@@ -20,6 +22,24 @@ class RecipeService {
       });
 
       return recipe;
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+
+  static async search(filter) {
+    try {
+      const recipes = await Recipe.findAll({
+        where: {
+          title: {
+            [Op.iLike]: `%${filter}%`,
+          },
+        },
+        order: [['updated_at', 'DESC']],
+        include: [{ association: 'chef' }, { association: 'files' }],
+      });
+
+      return recipes;
     } catch (error) {
       throw Error(error);
     }
