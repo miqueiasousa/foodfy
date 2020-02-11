6388fde0f483eb8328b2b441665e69e6e8bef94f
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-11 15:38:37 -0300

    Create chef service

---
+++ src/services/ChefService.js
@@ -0,0 +1,36 @@
+const Chef = require('../models/Chef');
+
+class ChefService {
+  static async findAll() {
+    try {
+      const chefs = await Chef.findAll({
+        order: [['created_at', 'DESC']],
+        include: { association: 'file' },
+      });
+
+      return chefs;
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+
+  static async findOne(id) {
+    try {
+      const chef = await Chef.findByPk(id, {
+        include: [
+          {
+            association: 'recipes',
+            include: [{ association: 'chef' }, { association: 'files' }],
+          },
+          { association: 'file' },
+        ],
+      });
+
+      return chef;
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+}
+
+module.exports = ChefService;
